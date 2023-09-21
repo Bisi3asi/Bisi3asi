6a19b5aea770b0ee0b6b69e67ffd42b6e7ccc35f
Bisi3asi <kueeng8008@naver.com>
2023-09-21 13:49:12 +0900

    MOD : few files fixes about algorithm

--- Java/Algorithm/COTE/src/LearnMethod.java
+++ Java/Algorithm/COTE/src/LearnMethod.java
@@ -1,9 +1,62 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class LearnMethod {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("good night");
-        sb.reverse();
-        System.out.println(sb.toString());
-        // out : "thgin doog"
+        // IntStream, LongStream
+        IntStream.range(1, 10).forEach(System.out::println);
+        // out : 1 ~ 9
+        IntStream.rangeClosed(1, 10).forEach(System.out::println);
+        // LongStream.range(1, 10000000).forEach(System.out::println); //
+        // out : 1 ~ 9999999 (DO NOT TRY THIS AT HOME)
+
+        // 오토박싱 : primitive 타입의 경우 오토박싱 처리 할 시 오버헤드 성능저하 우려
+        // 제너릭을 이용한 클래스로 사용하려면 박싱을 해서 사용해야한다.
+        Stream<Integer> stream1 = IntStream.range(1, 10).boxed(); // 박싱 적용
+        IntStream stream2 = IntStream.range(1, 10); // 언박싱
+
+        // filter 메소드
+        List<String> list = Arrays.asList("One", "One", "Two", "Three", "Four");
+        list.stream()
+            .filter(t ->t.contains("o"))
+            .forEach(System.out::println);
+        // out : "Two" "Four"
+
+        list.stream()
+            .filter(t -> t.equals("One"))
+            .forEach(System.out::println);
+        // out : "One" "One"
+
+        list.stream()
+            .filter(c -> c.endsWith("e"))
+            .forEach(System.out::println);
+        // out : "One" "One" "Three"
+
+        list.stream()
+            .filter(t -> t.length() >= 5)
+            .forEach(System.out::println);
+        // out : "Three"
+
+        // distinct
+        list.stream()
+            .distinct()
+            .forEach(System.out::println);
+        // out : "One" "Two" "Three" "Four"
+
+        // sort
+        list.stream()
+            .sorted()
+            .forEach(System.out::println);
+        // out : "Four" "One" "One" "Three" "Two"
+
+        list.stream()
+            .map(t -> t+"ayo")
+            .forEach(System.out::println);
+
+        String[] str = stream1.map(String::valueOf).toArray(String[]::new);
+
     }
+
 }
--- Java/Algorithm/COTE/src/Main.java
+++ Java/Algorithm/COTE/src/Main.java
@@ -1,14 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 class Main{
-    public String solution(String str) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < str.length(); i++){
-            if(Character.isAlphabetic(str.charAt(i))) {
-                sb.append(Character.toUpperCase(str.charAt(i)));
-            }
+    public String solution(int n, int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > arr[i-1])
+                list.add(arr[i]);
         }
-        String temp = sb.toString();
-        return (temp.contentEquals(sb.reverse()) ? "YES" : "NO");
+        StringBuilder answer = new StringBuilder();
+        for(int i = 0; i < list.size(); i++) {
+            answer.append(list.get(i));
+            if (i != list.size() - 1) answer.append(" ");
+        }
+        return answer.toString();
     }
     public static void main(String[] args) {
         // 실행시간 측정
@@ -16,8 +23,10 @@ class Main{
         // 측정 코드 시작
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println((T.solution(str)));
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        System.out.println((T.solution(n, arr)));
         // 측정 코드 끝
         // long afterTime = System.currentTimeMillis();
         // long diffTime = afterTime-beforeTime;
