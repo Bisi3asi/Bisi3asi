3316c4b46972ec6ba16b0638c7f794a2f1b479d0
Bisi3asi <kueeng8008@naver.com>
2023-09-30 01:40:23 +0900

    FEAT(TILjava) : Learning about java access modifier

---
+++ Java/TILjava/AccessModifierTest/Package1/ClassP1.java
@@ -0,0 +1,34 @@
+package JavaPractice.AccessModifierTest.Package1;
+import JavaPractice.AccessModifierTest.Package2.ClassP2;
+
+public class ClassP1 extends ClassP2{ // ClassP2 : 외부 패키지의 클래스
+    public static void main(String[] args) {
+        ClassP2 instance1 = new ClassP2(); // ClassP1 : 외부 패키지의 클래스를 상속받은 자식 클래스
+        instance1.publicMethod();
+        instance1.protectedMethod(); // 컴파일 에러 : protected는 외부 패키지에서의 접근 불가
+        instance1.defaultMethod(); // 컴파일 에러 : default는 외부 패키지에서의 접근 불가
+        instance1.privateMethod(); // 컴파일 에러 : private은 해당 클래스에서만 접근 가능
+
+        ClassP1 instance2 = new ClassP1();
+        instance2.publicMethod();
+        instance2.protectedMethod();
+        instance2.defaultMethod(); // 컴파일 에러 : default는 외부 패키지에서의 접근 불가
+        instance2.privateMethod(); // 컴파일 에러 : private은 해당 클래스에서만 접근 가능
+
+        ClassP2SonOuter instance3 = new ClassP2SonOuter();
+        instance3.protectedMethod(); // 컴파일 에러 :
+        /* [ClassP2SonOuter는 protected 접근이 안되고 ClassP1은 되는 이유]
+        * 공통점 : ClassP2와 외부 패키지이고 둘다 자식클래스이다.
+        * 차이점 : 선언된 접근제어자가 default, public으로 다르다
+        * 결론 :
+            1. public으로 선언된 ClassP1은 외부 패키지(Package1)에서
+            protected 멤버가 있는 패키지(Package2)로 접근 가능
+            2. default로 선언된 ClassP2SonOuter는 외부 패키지(Package1)에서
+            protected 멤버가 있는 패키지(Package2)로 접근 불가 : 외부 패키지이므로ㅜ
+            */
+    }
+}
+class ClassP2SonOuter extends ClassP2{}
+// ClassP2SonOuter : 외부 패키지의 클래스를 상속 받은 자식 클래스(default)
+// 하지만 default로 선언되었기 때문에 접근 불가
+// default 멤버는 default 멤버 -> 외부 패키지, 외부 패키지 -> default 멤버로 접근 불가
---
+++ Java/TILjava/AccessModifierTest/Package2/ClassP2.java
@@ -0,0 +1,60 @@
+package JavaPractice.AccessModifierTest.Package2;
+
+/** 동일 패키지의 클래스를 상속받는 자식 클래스 **/
+class ClassP2Son extends ClassP2 { }
+
+/** 일반 동일 패키지의 클래스 **/
+public class ClassP2 {
+    public void publicMethod() {
+        System.out.println("I am public");
+    }
+
+    protected void protectedMethod() {
+        System.out.println("i am protected");
+    }
+
+    void defaultMethod() {
+        System.out.println("i am default");
+    }
+
+    private void privateMethod() {
+        System.out.println("i am private");
+    }
+
+    public static void main(String[] args) {
+
+        ClassP2 instance1 = new ClassP2();
+        instance1.publicMethod();
+        instance1.protectedMethod();
+        instance1.defaultMethod();
+        instance1.privateMethod();
+
+        ClassP2Son instance2 = new ClassP2Son();
+        instance2.publicMethod();
+        instance2.protectedMethod();
+        instance2.defaultMethod();
+        instance2.privateMethod(); // 컴파일 에러 : 같은 클래스의 멤버가 아님
+
+        DefaultClass defaultClass = new DefaultClass();
+        System.out.println(defaultClass.privateString); // 컴파일 에러 : public을 통한 private의 접근이 아님
+        System.out.println(defaultClass.getPrivateString()); // public을 통한 접근
+    }
+}
+/** private 멤버를 가지고 있는 클래스가 default로 선언된 경우 **/
+class DefaultClass {
+    private String privateString = "i am private in default class";
+    public String getPrivateString(){
+        return this.privateString;
+    }
+}
+public class Hello { // 컴파일 에러 : 한 개의 자바 파일에 두개의 public class는 불가능
+    private int h;
+}
+class A {
+    protected int a;
+    private void p(){}
+}
+public class B{} // 컴파일 에러 : public class가 java 파일 내 두개 존재
+// protected, private으로 클래스 선언 불가능
+protected class C{} // 컴파일 에러
+private class D{} // 컴파일 에러
