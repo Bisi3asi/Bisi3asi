1a87a56241482c64114ab6ecd0dc5ec9de378084
Bisi3asi <kueeng8008@naver.com>
2023-10-05 14:14:34 +0900

    FEAT(TILjava) : Learn about etc modifier
    1. static, private, abstract

--- Java/Algorithm/COTE/src/Main.java
+++ Java/Algorithm/COTE/src/Main.java
@@ -1,20 +1,27 @@
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 class Main {
     public int solution(int n, int k, int[] arr) {
-        int answer = 0, len = 0, cnt = 0, pl = 0;
-        for(int i = 0; i < n; i++){
-            if (arr[i] == 0) cnt++;
-            while (cnt > k) {
-                if (arr[pl] == 0) cnt--;
-                pl++;
-            }
-            len = i-pl+1;
-            if (answer < len) {
-                answer = len;
+        TreeSet<Integer> tset = new TreeSet<>(Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    tset.add(arr[i] + arr[j] + arr[l]);
+                }
             }
         }
-        return answer;
+        Iterator<Integer> iter = tset.iterator();
+        int answer = 0;
+        if (tset.size() < k) return -1;
+        else {
+            for (int i = 0; i < k; i++) {
+                answer = iter.next();
+            }
+            return answer;
+        }
     }
 
     public static void main(String[] args) {
@@ -22,11 +29,12 @@ class Main {
         // 측정 코드 시작
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
         System.out.println(T.solution(n, k, arr));
         sc.close();
         // 측정 코드 끝
--- Java/TILjava/AccessModifierTest/Package2/ClassP2.java
+++ Java/TILjava/AccessModifierTest/Package2/ClassP2.java
@@ -33,28 +33,30 @@ public class ClassP2 {
         instance2.publicMethod();
         instance2.protectedMethod();
         instance2.defaultMethod();
-        instance2.privateMethod(); // 컴파일 에러 : 같은 클래스의 멤버가 아님
+        // instance2.privateMethod(); // 컴파일 에러 : 같은 클래스의 멤버가 아님
 
         DefaultClass defaultClass = new DefaultClass();
-        System.out.println(defaultClass.privateString); // 컴파일 에러 : public을 통한 private의 접근이 아님
+        // System.out.println(defaultClass.privateString); // 컴파일 에러 : public을 통한 private의 접근이 아님
         System.out.println(defaultClass.getPrivateString()); // public을 통한 접근
     }
 }
 /** private 멤버를 가지고 있는 클래스가 default로 선언된 경우 **/
 class DefaultClass {
     private String privateString = "i am private in default class";
-    public String getPrivateString(){
+
+    public String getPrivateString() {
         return this.privateString;
     }
 }
-public class Hello { // 컴파일 에러 : 한 개의 자바 파일에 두개의 public class는 불가능
-    private int h;
-}
+
+// public class Hello { // 컴파일 에러 : 한 개의 자바 파일에 두개의 public class는 불가능
+//    private int h;
+//}
 class A {
     protected int a;
     private void p(){}
 }
-public class B{} // 컴파일 에러 : public class가 java 파일 내 두개 존재
+// public class B{} // 컴파일 에러 : public class가 java 파일 내 두개 존재
 // protected, private으로 클래스 선언 불가능
-protected class C{} // 컴파일 에러
-private class D{} // 컴파일 에러
+// protected class C{} // 컴파일 에러
+// private class D{} // 컴파일 에러
