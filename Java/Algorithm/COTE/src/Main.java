8638e77c306749b331070f73ce9a09bd70b07893
Bisi3asi <kueeng8008@naver.com>
2023-10-17 04:02:24 +0900

    Add(TILjava) : 어노테이션에 대한 이해와 구현

--- Java/Algorithm/COTE/src/Main.java
+++ Java/Algorithm/COTE/src/Main.java
@@ -1,25 +1,32 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
-    public int solution(int n, int m, int[] arr) {
-        for (int i = 1; i < n; i++) {
-            int temp = arr[i], j;
-            for (j = i - 1; j >= 0; j--) {
-                if (arr[j] > temp) arr[j + 1] = arr[j];
-                else break;
+    public int decision(int mid, int[] arr) {
+        int target = arr[arr.length - 1], count = 0;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (target - arr[i] >= mid) {
+                count++;
+                target = arr[i];
             }
-            arr[j + 1] = temp;
         }
-        // binary search
-        int answer = 0, lt = 0, rt = n-1;
-        while(lt <= rt){
+        return count;
+    }
+
+    public int solution(int n, int c, int[] arr) {
+        int lt = Integer.MAX_VALUE, rt = 0, answer = 0;
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (Math.abs(arr[i] - arr[i + 1]) < lt) lt = Math.abs(arr[i] - arr[i + 1]);
+        }
+        rt = Arrays.stream(arr).max().getAsInt() - Arrays.stream(arr).min().getAsInt();
+        while (lt <= rt) {
             int mid = (lt + rt) / 2;
-            if (arr[mid] == m) {
-                answer = mid+1;
-                break;
-            }
-            else if (arr[mid] > m) rt = mid-1;
-            else lt = mid+1;
+//            System.out.println("mid : " + mid + " lt : " + lt + " rt : " + rt);
+            if (decision(mid, arr) >= c - 1) {
+                answer = mid;
+                lt = mid + 1;
+            } else rt = mid - 1;
         }
         return answer;
     }
@@ -30,10 +37,10 @@ class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        int c = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
-        System.out.println(T.solution(n, m, arr));
+        System.out.println(T.solution(n, c, arr));
         sc.close();
         // 측정 코드 끝
         // long afterTime = System.currentTimeMillis();
