1437f8ea0024de09ed5cc97b74283e4dcfa1df6a
Bisi3asi <kueeng8008@naver.com>
2023-10-18 17:09:05 +0900

    MOD(TILjava) : 람다식에 대한 이해

--- Java/TILjava/LambdaTest.java
+++ Java/TILjava/LambdaTest.java
@@ -1,11 +1,9 @@
 package JavaPractice;
 
-import java.util.Arrays;
-
 public class LambdaTest {
     /**
-     * 인자값이 한개인 함수형 인터페이스
-     * void 로 출력하는 메소드 구현
+     * 인자 값이 없는 함수형 인터페이스
+     * 결과 값은 void로 출력하는 메소드를 구현한다
      */
     @FunctionalInterface
     public interface Par0 {
@@ -13,7 +11,8 @@ public class LambdaTest {
     }
 
     /**
-     * 인자 값이 1개인 함수형 인터페이스
+     * 입력 인자 값이 int 1개인 함수형 인터페이스
+     * 결과 값을 int로 리턴한다
      */
     @FunctionalInterface
     public interface Par1 {
@@ -21,7 +20,8 @@ public class LambdaTest {
     }
 
     /**
-     * 인자 값이 2개인 함수형 인터페이스
+     * 입력 인자 값이 int 2개인 함수형 인터페이스
+     * 결과 값을 int로 리턴한다
      */
     @FunctionalInterface
     public interface Par2 {
@@ -29,7 +29,8 @@ public class LambdaTest {
     }
 
     /**
-     * 인자 값이 1개, 제네릭 타입 T를 가진 함수형 인터페이스
+     * 입력 인자 값이 1개, 제네릭 타입 T를 가진 함수형 인터페이스
+     * 결과 값은 void로 출력하는 메소드를 구현한다
      */
     @FunctionalInterface
     public interface Gen1<T> {
@@ -58,12 +59,13 @@ public class LambdaTest {
             // 함수형 인터페이스 변수 = 람다식; 형태로 구현한다.
             Par2 ex2 = (x, y) -> x * 10 + y;
 
-            // 실행 및 호출 결과
+            // 호출 및 실행 결과
             int a = 5, b = 3;
             System.out.println(ex1.exec(5, 3)); // 익명 클래스
             // out : 53
             System.out.println(ex2.exec(5, 3)); // 람다식
             // out : 53
+
         /* 람다식의 소개 예제 끝 */
 
         /* 1. 람다식의 기본 문법 */
@@ -80,7 +82,7 @@ public class LambdaTest {
             // 인자값 타입이 추론 가능한 경우 생략 가능하다
             Par1 ex6 = (x) -> x + 10;
             // 단, 두 개 이상의 인자값을 받는 람다식의 경우 특정 매개변수 타입만 생략은 불가능하다
-            // Par2 ex7 = (int x, y) -> x + y; // error : y도 int로 선언하던지 둘다 생략하던지
+            // Par2 ex7 = (int x, y) -> x + y; // error : y도 int로 선언하던지 둘다 생략하던지 해야 함
 
             // 인자값이 한개일 때 괄호의 생략이 가능하다
             Par1 ex8 = x -> x + 10;
@@ -102,12 +104,15 @@ public class LambdaTest {
 
             // 람다식의 형변환
             // 람다식은 익명 객체이므로 타입이 없다 (함수형 인터페이스로 람다식을 참조할 뿐)
-            Par0 ex12 = () -> {};
+            Par1 ex12 = (abc) -> abc + 1;
             // 명시적 형변환(모든 인터페이스 타입 변수에 람다식 할당 행위는 캐스팅 연산자가 생략되어 있음)
-            Par0 ex13 = (Par0)() -> {};
+            Par1 ex13 = (Par1)(abc) -> abc + 1;
             // 이런 특징을 활용해 최상위 클래스인 Object로 업캐스팅이 가능하다
             // 그 과정에서 함수형 인터페이스로 반드시 변환해야 한다
-            Object ex14 = (Object)(Par0)() -> {};
+            Object ex14 = (Object)(Par1) abc -> abc + 1;
+
+
+
 
             // 람다식으로 구현한 함수형 인터페이스를 매개변수로 전달
             // 람다식을 매개변수로 전달 할 수 있는 것은 메소드를 메소드의 매개변수로 넣을 수 있음을 의미.
@@ -117,8 +122,17 @@ public class LambdaTest {
             method(100, 200, (x, y) -> x * y);
                 // out : 20000
 
-            // 람다의 메소드 호출
-            Par1 ex15 = x -> Math::abs;
+            // 람다의 메소드 참조
+            Par1 ex15 = Math::abs; // Math.abs(int a) 를 간략화
+            System.out.println(ex15.exec(-13));
+                // out: -13
+            Par2 ex16 = Integer::max; // Integer.max(int a, int b)를 간략화
+            System.out.println(ex16.exec(13,15));
+                // out: 15
+            Gen1<String> ex17 = System.out::println; // Sysout(String)을 간략화
+            ex17.print("Hello!");
+                // out: "Hello!"
 
+        /* 람다식의 기본 문법 끝 */
     }
 }
