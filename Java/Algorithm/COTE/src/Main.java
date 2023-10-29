d1d93f59225cd5859b55d53fc89fa83c729d34e3
Bisi3asi <kueeng8008@naver.com>
2023-10-29 23:26:00 +0900

    feat : DFS, fibonaci prob

--- Java/Algorithm/COTE/src/Main.java
+++ Java/Algorithm/COTE/src/Main.java
@@ -5,12 +5,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 class Main {
+    static int[] arr;
     public int DFS(int n) {
-        if (n == 1) {
-            return 1;
-        } else {
-            return n * DFS(n-1);
-        }
+        if (arr[n] > 0) return arr[n];
+        if (n == 1) return arr[n] = 1;
+        else if (n == 2) return arr[n] = 1;
+        else return arr[n] = DFS(n-2) + DFS(n-1);
     }
 
     public static void main(String[] args) throws IOException {
@@ -19,7 +19,10 @@ class Main {
         Main T = new Main();
         try {
             int n = Integer.parseInt(br.readLine());
-            bw.write(String.valueOf(T.DFS(n)));
+            arr = new int[n+1];
+            T.DFS(n);
+            for (int i = 1; i < n; i++)
+            bw.write(arr[i] +" ");
         } catch (IOException e) {
             throw new IOException(e);
         }
