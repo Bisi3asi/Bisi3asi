fda9308a974bcac5bf1f3a646353d031e17502fa
Bisi3asi <kueeng8008@naver.com>
2023-10-31 00:26:05 +0900

    feat : @ResponseBody, @RequestParam
    * @ResponseBody : String 리턴시와 객체 리턴시의 차이점
         -String : StringConverter로 String 반환,
         -객체 : JsonConverter로 Json 반환

--- Spring/TILSpring/mogakco/src/main/java/com/mogakco/mogakco/controller/HelloController.java
+++ Spring/TILSpring/mogakco/src/main/java/com/mogakco/mogakco/controller/HelloController.java
@@ -1,10 +1,10 @@
 package com.mogakco.mogakco.controller;
 
-import java.lang.annotation.Retention;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -23,12 +23,42 @@ public class HelloController {
         return "hello";
     }
 
-    @GetMapping("hello-mvc")
+    @GetMapping("hello-mvc") //localhost:8080/hello-mvc?name="String"
+    // RequestParam("name") : 사용자의 뷰로 name의 value 값을 받겠다
     // hello-mvc?name="String" 으로 접속 시 해당 "String"이 표출된다.
     // 컨트롤러가 view로 값을 던질 때 사용자가 주소창에 입력한 String 값을 통해
     // model(name:"String")으로 매핑해 키값은 name, value값은 String으로 리턴해주기 때문
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string") //localhost:8080/hello-string?name="String"
+    @ResponseBody // 응답 바디부에 내가 이 내용을 직접 넣겠다 라는 뜻
+    // 결과를 보면 html이나 view 없고, 해당 string이 그대로 내려옴
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api") //localhost:8080/hello-api?name="String"
+    @ResponseBody // responsebody가 있으면 viewresolver 안가고 http 응답에 그대로 데이터를 넘김
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+        // 객체 데이터 리턴 시 JsonConverter 작동 : 스프링은 객체 → JSON 형태 반환을 기본 정책으로 채택
+        // 단순 문자 리턴 시  StringConverter가 작동
+
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
