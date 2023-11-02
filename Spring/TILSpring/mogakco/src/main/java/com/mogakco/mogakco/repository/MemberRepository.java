7918ce2f77315ddd5913ceca595ec52a72150294
Bisi3asi <kueeng8008@naver.com>
2023-11-02 23:53:59 +0900

    feat : MemoryRepository(No DB)

---
+++ Spring/TILSpring/mogakco/src/main/java/com/mogakco/mogakco/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package com.mogakco.mogakco.repository;
+
+import com.mogakco.mogakco.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
