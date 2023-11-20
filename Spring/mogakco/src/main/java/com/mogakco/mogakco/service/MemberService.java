f45110c171f644dd2d7d30690172c62f564e428d
Bisi3asi <kueeng8008@naver.com>
2023-11-20 09:51:40 +0900

    feat : learn about static resource

--- Spring/mogakco/src/main/java/com/mogakco/mogakco/service/MemberService.java
+++ Spring/mogakco/src/main/java/com/mogakco/mogakco/service/MemberService.java
@@ -26,7 +26,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
             .ifPresent(m -> {
-            throw new IllegalArgumentException("이미 존재하는 회원입니다");
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
 
