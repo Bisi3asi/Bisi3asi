048e4c8de363398cd6a69e0ff83484cc2d3f3001
Bisi3asi <kueeng8008@naver.com>
2024-01-18 16:01:55 +0900

    feat: git app.js, root, index.css 이해
      * 빌드 시 npm run build
      * 배포 시 npx serve -s build

--- React/react-practice/src/App.js
+++ React/react-practice/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+// App을 기반으로 작동, 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Hello React!
     </div>
   );
 }
