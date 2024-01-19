eac33374b0c02948a2e4c0199095c90098c279bf
Bisi3asi <kueeng8008@naver.com>
2024-01-19 10:31:17 +0900

    learn: create

--- React/react-practice/src/App.js
+++ React/react-practice/src/App.js
@@ -1,20 +1,43 @@
 import "./App.css";
 import { useState } from "react"; // 상태 작성을 위한 import
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
+
 // 매개변수로 props를 던져준다.
 function Header(props) {
   return (
     <header>
       <h1>
-        {/* function(event) 는 (event) => 로 유사 람다 지원 */}
         <a
           href="/"
           onClick={(event) => {
             event.preventDefault(); // 클릭해도 이벤트가 일어나지 않게 설정
             props.onChangeMode(); // props의 이벤트도 프로퍼티로 줘서 사용
-          }}
-        >
-          {props.title}
+          }}>{props.title}
         </a>
       </h1>
     </header>
@@ -63,19 +86,22 @@ function App() {
   const [mode, setMode] = useState("WELCOME");
 
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
-  ];
+  ]);
 
   let content = null;
+
   if (mode === "WELCOME") {
     content = (
       <Article title="WELCOME!" description="Web is Stronger than me"></Article>
     );
   } else if (mode === "READ") {
-    let title, body = null;
+    let title,
+      body = null;
     for (let i = 0; i < topics.length; i++) {
       if (topics[i].id === id) {
         title = topics[i].title;
@@ -83,6 +109,20 @@ function App() {
       }
     }
     content = <Article title={title} description={body}></Article>;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics] // 객체의 경우에는 ... 으로 복제본을 만든다
+          newTopics.push(newTopic);
+          setTopics(newTopics); // set할때 값이 다르면 다시 렌더링을 한다.
+          setMode('READ');
+          setId(nextId);
+          setNextId(nextId+1);
+        }}
+      ></Create>
+    );
   }
 
   return (
@@ -101,6 +141,18 @@ function App() {
         }}
       ></Nav>
       {content}
+      <ul>
+        <li>
+          <a href="/create" onClick={(event) => {
+              event.preventDefault();
+              setMode("CREATE");
+            }}>Create
+          </a>
+        </li>
+        <li>
+          <a href="/update">Update</a>
+        </li>
+      </ul>
     </div>
   );
 }
