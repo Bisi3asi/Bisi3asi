a23fcb6fc30e4ba74982488878d071654d02b8d8
Bisi3asi <kueeng8008@naver.com>
2024-01-19 09:47:11 +0900

    learn: State에 대한 이해

--- React/react-practice/src/App.js
+++ React/react-practice/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react"; // 상태 작성을 위한 import
 
 // 매개변수로 props를 던져준다.
 function Header(props) {
@@ -8,7 +9,7 @@ function Header(props) {
         {/* function(event) 는 (event) => 로 유사 람다 지원 */}
         <a
           href="/"
-          onClick={event => {
+          onClick={(event) => {
             event.preventDefault(); // 클릭해도 이벤트가 일어나지 않게 설정
             props.onChangeMode(); // props의 이벤트도 프로퍼티로 줘서 사용
           }}
@@ -36,10 +37,16 @@ function Nav(props) {
     let t = props.topics[i];
     lis.push(
       <li key={t.id}>
-        <a id = {t.id} href={"/read/" + t.id} onClick={event => {
-          event.preventDefault();
-          props.onChangeMode(event.target.id); // event.target : 특정 이벤트를 유발시킨 태그
-        }}>{t.title}</a>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id)); // event.target : 특정 이벤트를 유발시킨 태그
+          }}
+        >
+          {t.title}
+        </a>
       </li>
     );
   }
@@ -52,28 +59,48 @@ function Nav(props) {
 }
 
 function App() {
+  // mode(data, function) data : 상태 정의 데이터, function : 상태 변경을 위한 함수
+  const [mode, setMode] = useState("WELCOME");
+
+  const [id, setId] = useState(null);
   const topics = [
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "javascript is ..." },
   ];
 
+  let content = null;
+  if (mode === "WELCOME") {
+    content = (
+      <Article title="WELCOME!" description="Web is Stronger than me"></Article>
+    );
+  } else if (mode === "READ") {
+    let title, body = null;
+    for (let i = 0; i < topics.length; i++) {
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} description={body}></Article>;
+  }
+
   return (
     <div>
-      {/* props의 title이라는 프로퍼티를 정의한다. 함수 매개변수 처럼 이용 */}
       <Header
         title="REACT"
         onChangeMode={() => {
-          alert("Header");
+          setMode("WELCOME");
         }}
       ></Header>
-      <Article title="WELCOME!" description="Web is Stronger than me"></Article>
       <Nav
         topics={topics}
-        onChangeMode={(id) => {
-          alert(id);
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
         }}
       ></Nav>
+      {content}
     </div>
   );
 }
